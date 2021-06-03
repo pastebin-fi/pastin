9263ab70ac9a126611c85bb565dc3c5164f42399
Asdew95 <asdew@tutanota.com>
2021-06-03 19:35:37 +0300

    Forgot paste.py

--- paste.py
+++ paste.py
@@ -18,7 +18,7 @@ class Paste(db.Model):
     id = db.Column(db.String(16), primary_key=True)
     content = db.Column(db.UnicodeText)
     # TODO: Localize default name
-    name = db.Column(db.UnicodeText, default="Nimetön paste")
+    name = db.Column(db.String(1048576), default="Nimetön paste")
     private = db.Column(db.Boolean, default=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
