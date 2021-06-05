2fad5297d5c464d74e0d582d52c6a5ebea7bb91c
Asdew95 <asdew@tutanota.com>
2021-06-05 21:33:19 +0300

    Small fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_hcaptcha import hCaptcha
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
@@ -14,5 +15,6 @@ hcaptcha = hCaptcha(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 md = Markdown(app, output_format='html4')
+login_manager = LoginManager(app)
 
 document_manager = DocumentManager(app, "documents/documents.json")
--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -6,7 +7,7 @@ from main import db, login_manager
 class User(UserMixin, db.Model):
     username = db.Column(db.String(32), primary_key=True, unique=True)
     # RFC 5321, an errata against RFC 3696, states that the maximum length of
-    # an email address 254.
+    # an email address is 254.
     email = db.Column(db.String(254), unique=True)
     pwd = db.Column(db.String(128))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
